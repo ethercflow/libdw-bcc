87c6bd83b50b5161c7e48d3646e76d547930b039
Zwb <ethercflow@gmail.com>
2019-05-18 10:24:17 +0800

    add ebpf prog: get_unwind_ctx

---
+++ ebpf_get_unwind_ctx.c
@@ -0,0 +1,88 @@
+#include <linux/sched.h>
+#include <uapi/linux/ptrace.h>
+
+#ifndef __inline
+# define __inline                               \
+        inline __attribute__((always_inline))
+#endif
+
+struct key_ {
+        u64 ts;
+        u64 id;
+};
+
+struct unwind_ctx {
+        u64 ts;
+        u32 tid;
+        u32 tgid;
+
+        struct pt_regs uregs;
+        char name[TASK_COMM_LEN];
+
+        int size;
+        char data[STACK_SIZE];
+};
+
+BPF_ARRAY(zero, struct unwind_ctx, 1);
+BPF_HASH(cache, struct key_, struct unwind_ctx);
+
+BPF_PERF_OUTPUT(unwind_ctxs);
+
+static __inline int get_unwind_ctx(struct pt_regs *ctx)
+{
+        struct pt_regs *user_regs = NULL;
+        struct task_struct *task = NULL;
+        struct unwind_ctx *zuc = NULL;
+        struct unwind_ctx *uc = NULL;
+        struct key_ k = {};
+        void *sp = NULL;
+        int ret = 0;
+        int z = 0;
+
+        task = (struct task_struct *)bpf_get_current_task();
+        if (user_mode(ctx)) {
+                user_regs = ctx;
+        } else {
+                if (task->mm)
+                        user_regs = ((struct pt_regs *)(task)->thread.sp0 - 1);
+        }
+
+        if (!user_regs)
+                return -1;
+
+        ret = bpf_probe_read(&sp, sizeof(sp), &user_regs->sp);
+        if (ret < 0)
+                return -1;
+
+        zuc = zero.lookup(&z);
+        if (!zuc)
+                return -1;
+
+        k.ts = bpf_ktime_get_ns();
+        k.id = bpf_get_current_pid_tgid();
+
+        uc = cache.lookup_or_init(&k, zuc);
+        if (!uc)
+                return -1;
+
+        uc->ts = k.ts;
+        uc->tgid = k.id >> 32;
+        uc->tid = k.id;
+        ret = bpf_probe_read(&uc->uregs, sizeof(uc->uregs), user_regs);
+        if (ret < 0)
+                return -1;
+        bpf_get_current_comm(&uc->name, sizeof(uc->name));
+        ret = bpf_probe_read_stack(&uc->data, sizeof(uc->data), sp);
+        if (ret < 0)
+                return -1;
+        if (ret == 0)
+                uc->size = STACK_SIZE;
+        else
+                uc->size = STACK_SIZE - ret;
+
+        unwind_ctxs.perf_submit(ctx, uc, sizeof(*uc));
+
+        cache.delete(&k);
+
+        return 0;
+}
