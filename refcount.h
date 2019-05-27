60e0a3126a67368a39e9df4a188b76f168dfb296
Zwb <ethercflow@gmail.com>
2019-05-27 15:31:26 +0800

    format refcount

--- refcount.h
+++ refcount.h
@@ -2,9 +2,31 @@
 #define __REFCOUNT_H_
 
 #include <stdatomic.h>
+#include <stdbool.h>
 
 typedef struct refcount_struct {
-        atomic_uint refs;
+     atomic_uint refs;
 } refcount_t;
+
+static inline void refcount_set(refcount_t *r, unsigned int n)
+{
+     atomic_store(&r->refs, n);
+}
+
+static inline unsigned int refcount_read(refcount_t *r)
+{
+     return atomic_load(&r->refs);
+}
+
+static inline void refcount_inc(refcount_t *r)
+{
+     atomic_fetch_add(&r->refs, 1);
+}
+
+static inline bool refcount_dec_and_test(refcount_t *r)
+{
+     /* TODO: impl this */
+     return false;
+}
 
 #endif // __REFCOUNT_H_
