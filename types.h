a301494ae7f8d287ec255461a7d397337b0ebbef
Zwb <ethercflow@gmail.com>
2019-05-27 15:30:53 +0800

    update types

--- types.h
+++ types.h
@@ -2,6 +2,7 @@
 #define __TYPES_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint64_t u64;
 typedef int64_t s64;
@@ -14,5 +15,17 @@ typedef int16_t s16;
 
 typedef uint8_t u8;
 typedef int8_t s8;
+
+struct list_head {
+    struct list_head *next, *prev;
+};
+
+struct hlist_head {
+    struct hlist_node *first;
+};
+
+struct hlist_node {
+    struct hlist_node *next, **pprev;
+};
 
 #endif // __TYPES_H_
